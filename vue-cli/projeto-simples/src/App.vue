c3b4fd6ad9c9ed3560ed64edd3d47492c9c5ff89
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-02-28 16:22:21 -0300

    VueCli introduction

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>{{ titulo }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      titulo: 'Teste Data usando Vue JS!!',
+    }
+  }
+}
+
+</script>
+
+<style>
+  #app {
+    background-color: red;
+  }
+</style>
